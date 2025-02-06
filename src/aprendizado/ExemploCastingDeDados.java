1686c3e9857649d6126e3dfd2163eb3161bfd631
Jose Jacsan <jjacsan@gmail.com>
2025-02-06 15:35:31 -0300

    Aprendendo casting de dados

---
+++ src/aprendizado/ExemploCastingDeDados.java
@@ -0,0 +1,15 @@
+package aprendizado;
+
+public class ExemploCastingDeDados {
+    public static void main(String[] args) {
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+
+        resultado = (double) a / b;
+
+        System.out.println(resultado);
+    }
+}
---
+++ src/aprendizado/ExemploCastingDeDados2.java
@@ -0,0 +1,13 @@
+package aprendizado;
+
+public class ExemploCastingDeDados2 {
+    public static void main (String[] args) {
+        double a;
+        int b;
+
+        a = 5.0;
+        b = (int) a;
+
+        System.out.println(b);
+    }
+}
