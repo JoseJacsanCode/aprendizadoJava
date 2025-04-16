54db5bed6c011112b1a5286785d901c393dd51bf
Jose Jacsan <jjacsan@gmail.com>
2025-04-15 21:59:28 -0300

    Exercício com while

---
+++ src/exercicios/Contagem.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+public class Contagem {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x <= 5 ) {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
+
+/*
+ * Use while para imprimir os números de 1 a 5 na tela, um por linha.
+1
+2
+3
+4
+5
+ */
