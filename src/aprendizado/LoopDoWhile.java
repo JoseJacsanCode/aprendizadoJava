b53b94e8de576c1f17030664ec3aac375d046dfa
Jose Jacsan <jjacsan@gmail.com>
2025-04-29 18:47:07 -0300

    Arquivos apagados e do-while e while feitos

---
+++ src/aprendizado/LoopDoWhile.java
@@ -0,0 +1,15 @@
+package aprendizado;
+
+public class LoopDoWhile {
+    public static void main(String[] args) {
+        int i = 0;
+
+        do {
+            i++;
+            System.out.println("Valor de " + i);
+
+        } while (i <= 15);
+
+        System.out.println(i);
+    }
+}
