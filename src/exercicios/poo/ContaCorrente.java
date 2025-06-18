2f058c75c76cc27eca7a4e83eecd461bedf55318
Jose Jacsan <jjacsan@gmail.com>
2025-06-18 16:12:41 -0300

    Exercício Conta Corrente corrigido

--- src/exercicios/poo/ContaCorrente.java
+++ src/exercicios/poo/ContaCorrente.java
@@ -5,7 +5,46 @@ public class ContaCorrente {
     String agencia;
     boolean especial;
     double limiteChequeEspecial;
+    double valorEspecialUsado;
     double saldo;
+
+    boolean realizarSaque(double quantiaASacar) {
+
+        // Tem saldo na conta
+        if (saldo >= quantiaASacar) {
+            saldo -= quantiaASacar;
+            return true;
+        }
+        else { // Não tem saldo na conta
+            if(especial) {
+                // Verificar se o limite especial tem saldo
+                double limite = limiteChequeEspecial + saldo;
+                if(limite >= quantiaASacar) {
+                    saldo -= quantiaASacar;
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    void depositar(double valorDepositado) {
+        saldo += valorDepositado;
+    }
+
+    void consultarSaldo() {
+        System.out.println("Saldo atual da conta = " + saldo);
+    }
+
+    boolean verificarUsoChequeEspecial() {
+        return saldo < 0;
+    }
+
 }
 
 /*
--- src/exercicios/poo/TesteContaCorrente.java
+++ src/exercicios/poo/TesteContaCorrente.java
@@ -7,8 +7,50 @@ public class TesteContaCorrente {
         conta.numero = "123456";
         conta.especial = true;
         conta.limiteChequeEspecial = 500;
-        conta.saldo = 10;
+        conta.saldo = -10;
 
         System.out.println("Saldo da conta " + conta.numero + " = " + conta.saldo);
+
+        boolean saqueEfetuado = conta.realizarSaque(10);
+
+        if(saqueEfetuado) {
+            System.out.println("Saque efetuado com sucesso");
+            conta.consultarSaldo();
+        }
+        else {
+            System.out.println("Não foi possível realizar saque, saldo insuficiente");
+        }
+
+        saqueEfetuado = conta.realizarSaque(500);
+
+        System.out.println("Tetativa de saque de 500 reais");
+        if(saqueEfetuado) {
+            System.out.println("Saque efetuado com sucesso");
+            conta.consultarSaldo();
+        }
+        else {
+            System.out.println("Não foi possível realizar saque, saldo insuficiente");
+        }
+
+        System.out.println("Depósito de 500 reais");
+        conta.depositar(500);
+    
+        conta.consultarSaldo();
+
+        if (conta.verificarUsoChequeEspecial()) {
+            System.out.println("Está usando cheque especial");
+        }
+        else {
+            System.out.println("Não está usando cheque especial");
+        }
+
+        conta.realizarSaque(600);
+        conta.consultarSaldo();
+        if (conta.verificarUsoChequeEspecial()) {
+            System.out.println("Está usando cheque especial");
+        }
+        else {
+            System.out.println("Não está usando cheque especial");
+        }
     }
 }
