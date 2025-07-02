1ff0d6da334300c0d0640f2e3fdb4f708089326b
Jose Jacsan <jjacsan@gmail.com>
2025-07-02 14:55:26 -0300

    Somatório

--- src/exercicios/CalculadoraRecursiva.java
+++ src/exercicios/CalculadoraRecursiva.java
@@ -9,4 +9,14 @@ public class CalculadoraRecursiva {
         
         return fibonnaci(num - 1) + fibonnaci(num - 2);
     }
+ 
+    public static int somatorio(int num) {
+
+        if(num == 1) {
+            return 1;
+        }
+
+        return num + somatorio(num - 1);
+    }
+
 }
--- src/exercicios/TesteCalculadoraRecursiva.java
+++ src/exercicios/TesteCalculadoraRecursiva.java
@@ -3,5 +3,6 @@ package exercicios;
 public class TesteCalculadoraRecursiva {
     public static void main(String[] args) {
         System.out.println(CalculadoraRecursiva.fibonnaci(3));
+        System.out.println(CalculadoraRecursiva.somatorio(5));
     }
 }
