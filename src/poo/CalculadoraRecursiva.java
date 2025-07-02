7edcf32317f8ffc7f5bbfb3c4cdccc8fc064330f
Jose Jacsan <jjacsan@gmail.com>
2025-07-02 14:47:13 -0300

    Recursividade e fibonnaci

---
+++ src/poo/CalculadoraRecursiva.java
@@ -0,0 +1,12 @@
+package poo;
+
+public class CalculadoraRecursiva {
+  
+    public static int fibonnaci(int num) {
+        if(num < 2) {
+            return 1;
+        }
+        
+        return fibonnaci(num - 1) + fibonnaci(num - 2);
+    }
+}
---
+++ src/poo/TesteCalculadoraRecursiva.java
@@ -0,0 +1,7 @@
+package poo;
+
+public class TesteCalculadoraRecursiva {
+    public static void main(String[] args) {
+        System.out.println(CalculadoraRecursiva.fibonnaci(3));
+    }
+}
